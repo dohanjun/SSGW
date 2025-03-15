6aa7f04e41b84515e71a761821bc7911cc2e5b64
sinyoung261 <waterfired@naver.com>
2025-03-15 13:04:53 +0900

    20250315-이신영 메일 목록 기능 출력

--- group/src/main/java/com/yedam/app/group/service/MailService.java
+++ group/src/main/java/com/yedam/app/group/service/MailService.java
@@ -1,11 +1,28 @@
 package com.yedam.app.group.service;
 
+import java.util.List;
+
 public interface MailService {
 	
 	//메일전체조회
+	public List<MailVO> findAll();
 	
 	//메일상세조회
+	public MailVO findMailId();
+	
+	//나의 메일상세조회
+	public MailVO MyFindMailId();
 	
 	//메일등록
+	public int addInfo(MailVO mailVO);
+	
+	//메일수정
+	public int UpdInfo(MailVO mailVO);
+	
+	//메일답장
+	public int PutInfo(MailVO mailVO);
+	
+	//메일삭제
+	public int DelInfo(MailVO mailVO);
 	
 }
