d278cf5b5317b25888f11904cc1e4e79b8d26df2
immipis <132124793+immipis@users.noreply.github.com>
2025-03-15 16:21:46 +0900

    일단 오케이

---
+++ group/src/main/java/com/yedam/app/group/service/SubscriberService.java
@@ -0,0 +1,5 @@
+package com.yedam.app.group.service;
+
+public interface SubscriberService {
+    void saveSubscriber(SubscriberVO subscriber);
+}
---
+++ group/src/main/java/com/yedam/app/group/service/impl/SubscriberServiceImpl.java
@@ -0,0 +1,22 @@
+package com.yedam.app.group.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.yedam.app.group.mapper.SubscriberMapper;
+import com.yedam.app.group.service.SubscriberService;
+import com.yedam.app.group.service.SubscriberVO;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class SubscriberServiceImpl implements SubscriberService {
+    
+    private final SubscriberMapper subscriberMapper;
+
+    @Override
+    public void saveSubscriber(SubscriberVO subscriber) {
+        subscriberMapper.insertSubscriber(subscriber);
+    }
+
+}
