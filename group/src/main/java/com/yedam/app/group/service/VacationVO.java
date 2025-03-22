fc96508bafe315d5d65a7157f0e3780d93bdc8a7
zh <zhwwn2@gmail.com>
2025-03-22 15:25:10 +0900

    03-22 퇴직처리기능 휴가자동부여 기능구현중 접근권한방식 고민중

--- group/src/main/java/com/yedam/app/group/service/VacationVO.java
+++ group/src/main/java/com/yedam/app/group/service/VacationVO.java
@@ -1,13 +1,23 @@
 package com.yedam.app.group.service;
 
+import java.util.Date;
+
 import lombok.Data;
 
 @Data
 public class VacationVO {
-	private Integer vacationTypeId; 			// 휴가유형
-	private String vacationTypeName;			// 휴가이름
-	private int vacationDate;					// 휴가일수
-	private String requiredProofDocumentFile;	// 증빙서류 필요여부
-	private int suberNo;						// 회사번호
+	private Integer vacationTypeId; 				// 휴가유형
+	private String vacationTypeName;				// 휴가이름
+	private Integer vacationDate;					// 휴가일수
+	private String requiredProofDocumentFile;		// 증빙서류 필요여부
+	private Integer suberNo;						// 회사번호
+	
+	private Integer grantedVacation;		// 부여된 휴가일
+	private Integer userVacation;			// 사용한 휴가일
+	private Integer remainingVacation;		// 휴가 잔여일
+	private Integer emplkoyeeNo;			// 사원번호
+	private Integer leaveHistoryId;			// 휴가내역번호
+	private Date Yeat;						// 연도
+	private Integer draftNo;				// 기안문서번호
 
 }
---
+++ group/src/main/java/com/yedam/app/group/service/impl/LeaveHistServiceimpl.java
@@ -0,0 +1,16 @@
+package com.yedam.app.group.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.yedam.app.group.mapper.VacationMapper;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class LeaveHistServiceimpl {
+	
+	private final VacationMapper vacationMapper;
+
+	
+}
